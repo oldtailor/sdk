80ed488389c618634415f4629fc2262c194c6afa
oldtailor <1058776960@qq.com>
2018-01-01 12:13:23 +0800

    add enum

---
+++ src/Enum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace oldtailor\sdk;
+
+class Enum {
+	
+	const USER_TYPE_UNKNOWN = 1;
+	const USER_TYPE_ADMIN   = 2;
+	const USER_TYPE_MEMBER  = 4;
+	const USER_TYPE_STORE   = 8;
+	
+	
+}
